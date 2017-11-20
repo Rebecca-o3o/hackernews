b242a1d64e68ea905d60ed5e3cebef46e812fa43
beccyo3o <rebecca.krummel@gmail.com>
2017-11-20 10:46:53 +0100

    import topstories and navigation to app

--- hn/src/App.js
+++ hn/src/App.js
@@ -1,21 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Topstories from './Topstories'
+import Navigation from './Navigation'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+
+          <h1 className="App-title">Hackernews styled</h1>
+          <Navigation />
+
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <Topstories />
+
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
