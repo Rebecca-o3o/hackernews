7f7bcf629000698471dbe7065ec7dcb581fe80fb
beccyo3o <rebecca.krummel@gmail.com>
2017-11-20 10:37:33 +0100

    fetch and display hn topstories

---
+++ hn/src/Topstories.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react'
+import './Topstories.css'
+
+const hnPrefix = "https://hacker-news.firebaseio.com/v0/"
+
+export default class Topstories extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      stories: []
+    }
+  }
+
+  componentDidMount() {
+
+    //fetch top stories
+    try {
+      fetch(hnPrefix + 'topstories.json?print=pretty')
+        .then(res => res.json())
+        .then((ids) => {
+
+          //TODO: variable max amount
+          ids.slice(0, 40).forEach((id) => {
+
+            //fetch and add story details to ids:
+            fetch(hnPrefix + `/item/${id}.json?print=pretty`)
+              .then(item => item.json())
+              .then((story) => {
+
+                // console.log(story)
+
+                this.setState({
+                  stories: this.state.stories.concat([story])
+                })
+              })
+          })
+        })
+
+    } catch (e) {
+      this.setState({
+        errorMessage: 'Error getting stories from hackernews'
+      })
+    }
+  }
+
+
+  render() {
+    if (!this.state.stories) {
+      return <div>Loading...</div>
+    }
+
+    const renderStories = () => {
+      return this.state.stories.map(story => {
+        return (
+          <div className="story-container">
+            <div className="title">{story.title}</div>
+            <div className="author">Author: {story.by}</div>
+            <a href={`${story.url}`}>Go to Article</a>
+          </div>
+        )
+      })
+    }
+
+    return (
+      <div className="news">
+        {renderStories()}
+      </div>
+    )
+  }
+}
